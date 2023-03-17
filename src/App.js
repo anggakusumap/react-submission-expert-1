66509f673209e8af9c200285bf3f289be7da20e8
I Gede Angga Kusuma Putra <kusuma_putra@student.unud.ac.id>
2023-03-18 07:50:13 +0800

    Setup eslint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="text-8xl bg-red-500">
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
