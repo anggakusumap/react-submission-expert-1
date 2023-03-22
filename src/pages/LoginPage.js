72abb5f85a0abe5c27722f9933ce625f71556e64
I Gede Angga Kusuma Putra <kusuma_putra@student.unud.ac.id>
2023-03-23 00:15:41 +0800

    finish login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import LoginInput from '../components/LoginInput';
 import Navigation from '../components/Navigation';
+import { asyncSetAuthUser } from '../states/authUser/action';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onLogin = ({ email, password }) => {
+    dispatch(asyncSetAuthUser({ email, password }));
+    navigate('/');
+  };
+
   return (
     <>
       <Navigation />
-      <div>LoginPage</div>
+      <LoginInput login={onLogin} />
     </>
   );
 }
